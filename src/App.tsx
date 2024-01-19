a4442954a9cbd86db8a83cc6947b9cb9bd1598f7
Kendrick Cosca <kendrick@lingjack.com>
2024-01-19 15:53:15 +0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,172 @@
+import { ThemeProvider } from './components/theme-provider';
+import { Button } from './components/ui/button';
+import { ModeToggle } from './components/mode-toggle';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from './components/ui/card';
+import { Payment, columns } from './components/sample-table/columns';
+import { DataTable } from './components/sample-table/data-table';
+
+
+function App() {
+    const data: Payment[] = [
+        {
+            id: "728ed52f",
+            amount: 100,
+            status: "pending",
+            email: "m@example.com",
+        },
+        {
+            id: "728ed52f",
+            amount: 100,
+            status: "pending",
+            email: "a@example.com",
+        },
+        {
+            id: "728ed52f",
+            amount: 100,
+            status: "pending",
+            email: "bm@example.com",
+        },
+        {
+            id: "728ed52f",
+            amount: 100,
+            status: "pending",
+            email: "cm@example.com",
+        },
+        {
+            id: "728ed52f",
+            amount: 100,
+            status: "pending",
+            email: "dm@example.com",
+        },
+        {
+            id: "728ed52f",
+            amount: 100,
+            status: "pending",
+            email: "dm@example.com",
+        },
+        {
+            id: "728ed52f",
+            amount: 100,
+            status: "pending",
+            email: "dm@example.com",
+        },
+        {
+            id: "728ed52f",
+            amount: 100,
+            status: "pending",
+            email: "dm@example.com",
+        },
+        {
+            id: "728ed52f",
+            amount: 100,
+            status: "pending",
+            email: "dm@example.com",
+        },
+        {
+            id: "728ed52f",
+            amount: 100,
+            status: "pending",
+            email: "dm@example.com",
+        },
+        {
+            id: "728ed52f",
+            amount: 100,
+            status: "pending",
+            email: "dm@example.com",
+        },
+        {
+            id: "728ed52f",
+            amount: 100,
+            status: "pending",
+            email: "dm@example.com",
+        },
+        {
+            id: "728ed52f",
+            amount: 100,
+            status: "pending",
+            email: "dm@example.com",
+        },
+        {
+            id: "728ed52f",
+            amount: 100,
+            status: "pending",
+            email: "dm@example.com",
+        },
+        {
+            id: "728ed52f",
+            amount: 100,
+            status: "pending",
+            email: "dm@example.com",
+        },
+        {
+            id: "728ed52f",
+            amount: 100,
+            status: "pending",
+            email: "dm@example.com",
+        },
+        {
+            id: "728ed52f",
+            amount: 100,
+            status: "pending",
+            email: "dm@example.com",
+        },
+        {
+            id: "728ed52f",
+            amount: 100,
+            status: "pending",
+            email: "dm@example.com",
+        },
+        {
+            id: "728ed52f",
+            amount: 100,
+            status: "pending",
+            email: "dm@example.com",
+        },
+        {
+            id: "728ed52f",
+            amount: 100,
+            status: "pending",
+            email: "dm@example.com",
+        },
+        {
+            id: "728ed52f",
+            amount: 100,
+            status: "pending",
+            email: "dm@example.com",
+        },
+        {
+            id: "728ed52f",
+            amount: 100,
+            status: "pending",
+            email: "dm@example.com",
+        },
+        {
+            id: "728ed52f",
+            amount: 100,
+            status: "pending",
+            email: "dm@example.com",
+        },
+    ]
+
+    return (
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+            <ModeToggle />
+            <div className='container-full p-3'>
+                <Card>
+                    <CardHeader>
+                        <CardTitle>Card Title</CardTitle>
+                        <CardDescription>Card Description</CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <DataTable columns={columns} data={data} />
+                    </CardContent>
+                    <CardFooter>
+                        <Button>Button</Button>
+                    </CardFooter>
+                </Card>
+            </div>
+        </ThemeProvider>
+    );
+}
+
+export default App;
